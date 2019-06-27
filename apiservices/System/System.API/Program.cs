d6941a3066d776d6dc3e50da7172ccf74015f0cc
chengqf <chengqf@seari.com>
2019-06-27 18:33:15 +0800

    代码测试

--- apiservices/System/System.API/Program.cs
+++ apiservices/System/System.API/Program.cs
@@ -13,14 +13,14 @@ namespace System.API
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+        //public static void Main(string[] args)
+        //{
+        //    CreateWebHostBuilder(args).Build().Run();
+        //}
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>();
 
 
         public static void Main(string[] args)
