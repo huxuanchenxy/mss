12c2dbeb10f4b04f52c234f9376da917f8d690ad
hyx <huyanxinag@outlook.com>
2019-06-28 13:06:10 +0800

    operlog change route

--- frontend/MSS.API.UI/src/api/operlogApi.js
+++ frontend/MSS.API.UI/src/api/operlogApi.js
@@ -1,6 +1,6 @@
 import axios from './interceptors'
- let api = 'http://localhost:3851/api/v1'
-//let api = 'http://10.89.36.204:5801/operlogapi'
+ //let api = 'http://localhost:3851/api/v1'
+let api = 'http://10.89.36.204:5801/operlogapi'
 export default {
   getOperationLog: parm => { return axios.get(`${api}/UserOperationLog/QueryList`, {params: parm}).then(res => res.data) }
 }
