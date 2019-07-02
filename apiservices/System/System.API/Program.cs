0496d393429caa37444c73dee2aa665888f0c7c0
chengqf <chengqf@seari.com>
2019-07-02 11:01:59 +0800

    as

--- apiservices/System/System.API/Program.cs
+++ apiservices/System/System.API/Program.cs
@@ -13,40 +13,40 @@ namespace System.API
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-
-
         //public static void Main(string[] args)
         //{
-        //    BuildWebHost(args).Run();
+        //    CreateWebHostBuilder(args).Build().Run();
         //}
 
-        //public static IWebHost BuildWebHost(string[] args)
-        //{
-        //    int port = int.Parse(args[0]);//3861
-        //    return WebHost.CreateDefaultBuilder(args)
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>();
 
-        //        .UseContentRoot(Directory.GetCurrentDirectory())
-        //         .UseIISIntegration()
 
-        //        .UseKestrel(options =>
-        //        {
-        //            options.Listen(IPAddress.Any, port);
-        //            //options.Listen(IPAddress.Any, 443, listenOptions =>
-        //            //{
-        //            //    listenOptions.UseHttps("server.pfx", "password");
-        //            //});
-        //        })
-        //      .UseStartup<Startup>()
-        //        .Build();
-        //}
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            int port = int.Parse(args[0]);//3861
+            return WebHost.CreateDefaultBuilder(args)
+
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                 .UseIISIntegration()
+
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Any, port);
+                    //options.Listen(IPAddress.Any, 443, listenOptions =>
+                    //{
+                    //    listenOptions.UseHttps("server.pfx", "password");
+                    //});
+                })
+              .UseStartup<Startup>()
+                .Build();
+        }
 
     }
 }
