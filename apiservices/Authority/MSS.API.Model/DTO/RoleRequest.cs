76bf429f5443083ae4bcdd5a52165e03616d3228
twg <twg>
2019-07-16 13:43:01 +0800

    角色权限查询包含所有级别

--- apiservices/Authority/MSS.API.Model/DTO/RoleRequest.cs
+++ apiservices/Authority/MSS.API.Model/DTO/RoleRequest.cs
@@ -12,9 +12,14 @@ namespace MSS.API.Model.DTO
         /// </summary>
         public string searchName { get; set; }
         /// <summary>
-        /// action(级联)下拉查询
+        /// action(级联)下拉查询(非一级)
         /// </summary>
         public int? searchAction { get; set; }
+        /// <summary>
+        /// action(级联)下拉查询(一级)
+        /// </summary>
+        public int? searchActionGroup { get; set; }
+        
     }
 
     public class RoleStrActions : Role
