c03acd5835558c65cce6cdb64e0a14d1e40a2b3d
chengqf <chengqf@seari.com>
2019-08-01 17:56:57 +0800

    修改接口路径

--- frontend/MSS.API.UI/src/api/ExpertApi.js
+++ frontend/MSS.API.UI/src/api/ExpertApi.js
@@ -1,6 +1,6 @@
 import axios from './interceptors'
-// let api = 'http://10.89.36.204:5801/maintainapi'
-let api = 'http://localhost:3851/api/v1'
+let api = 'http://10.89.36.204:5801/maintainapi'
+// let api = 'http://localhost:3851/api/v1'
 export default {
   getAllUsers: () => { return {} },
   Save: data => { return axios.post(`${api}/ExpertData/Save`, data).then(res => res.data) },
