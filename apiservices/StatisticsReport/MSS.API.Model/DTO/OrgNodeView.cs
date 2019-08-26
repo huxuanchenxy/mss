3844d7ea2bb4569820e312d2d635f7703ea85f91
niefei <nief@seari.com>
2019-08-26 09:12:05 +0800

    报警后端

---
+++ apiservices/StatisticsReport/MSS.API.Model/DTO/OrgNodeView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSS.API.Model.DTO
+{
+    public class OrgNodeType
+    {
+        public int ID { get; set; }
+        public string TypeName { get; set; }
+        public bool HasChildren { get; set; }
+        public bool HasUsers { get; set; }
+        public bool HasUsersLeafOnly { get; set; }
+    }
+    public class OrgNodeView
+    {
+        public int id { get; set; }
+
+        public string label { get; set; }
+
+        public string node_type { get; set; }
+
+        public OrgNodeType type { get; set; }
+
+        public List<OrgNodeView> children { get; set; }
+        
+    }
+}
