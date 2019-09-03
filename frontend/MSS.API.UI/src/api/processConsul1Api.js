ae46c4c17d5f4454be9ab6940c9a15ba57c6ebf3
hyx <huyanxinag@outlook.com>
2019-09-03 15:11:58 +0800

    swith to 10.89.36.154:5801 gateway

--- frontend/MSS.API.UI/src/api/processConsul1Api.js
+++ frontend/MSS.API.UI/src/api/processConsul1Api.js
@@ -1,6 +1,6 @@
 import axios from './interceptors'
 // let api = 'http://10.89.36.160:8400/api/v1'
-let api = 'http://10.89.36.204:5801/consulapi103'
+let api = 'http://10.89.36.154:5801/consulapi103'
 export default {
   getPage: parm => { return axios.get(`${api}/Consul`, {params: parm}).then(res => res.data) },
   startProcess: id => { return axios.get(`${api}/Consul/start/${id}`).then(res => res.data) },
