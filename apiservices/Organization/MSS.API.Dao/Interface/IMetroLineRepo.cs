f79f5dc2be261c3aef07d56aa0fef52f0e05f7f3
niefei <nief@seari.com>
2019-09-23 14:53:45 +0800

    线路管理

---
+++ apiservices/Organization/MSS.API.Dao/Interface/IMetroLineRepo.cs
@@ -0,0 +1,30 @@
+﻿using MSS.API.Model.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MSS.API.Model.DTO;
+using MSS.API.Dao.Common;
+namespace MSS.API.Dao.Interface
+{
+    public interface IMetroLineRepo<T> where T:BaseEntity
+    {
+        // 保存线路
+        Task<MetroLine> SaveLine(MetroLine line);
+
+        Task<bool> CheckExist(MetroLine line);
+
+        Task<MetroLine> UpdateLine(MetroLine line);
+
+        Task<int> DeleteLine(List<MetroLine> line);
+
+        // 根据id查询
+        Task<MetroLine> GetMetroLineByID(int id);
+
+        // 分页查询
+        Task<PageData<MetroLine>> ListMetroLineByPage(MetroLineParam param);
+
+        // 所有地铁线
+        Task<List<MetroLine>> ListAllMetroLine();
+    }
+}
---
+++ apiservices/Organization/MSS.API.Model/Data/MetroLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+using Dapper.FluentMap.Mapping;
+namespace MSS.API.Model.Data
+{
+    public class MetroLine :BaseEntity
+    {
+        public string LineName { get; set; }
+        public string Description { get; set; }
+
+        public string UserName { get; set; }
+    }
+
+    public class MetroLineMap : EntityMap<MetroLine>
+    {
+        public MetroLineMap()
+        {
+            Map(o => o.LineName).ToColumn("line_name");
+            Map(o => o.Description).ToColumn("description");
+            Map(o => o.UserName).ToColumn("user_name");
+
+            Map(o => o.CreatedBy).ToColumn("created_by");
+            Map(o => o.CreatedTime).ToColumn("created_time");
+            Map(o => o.UpdatedBy).ToColumn("updated_by");
+            Map(o => o.UpdatedTime).ToColumn("updated_time");
+            Map(o => o.IsDel).ToColumn("is_del");
+        }
+    }
+}
