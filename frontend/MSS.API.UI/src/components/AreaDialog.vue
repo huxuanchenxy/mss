18aff26a6565e3f7848904a0e601af6dfeb371d5
hyx <huyanxinag@outlook.com>
2019-10-22 15:09:50 +0800

    areadialog

---
+++ frontend/MSS.API.UI/src/components/AreaDialog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="selfShow">
+    <button @click="changeShowChild" >to hidden</button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    changeShowChild () {
+      this.$emit('changeShowChild')
+    }
+  },
+  computed: {
+    selfShow () {
+      return this.$attrs.name
+    }
+  }
+}
+</script>
+<style>
+</style>
