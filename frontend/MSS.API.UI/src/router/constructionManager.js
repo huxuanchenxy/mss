bb0694e090b0c4a80679cfecbeee145d4cb33b89
hyx <huyanxinag@outlook.com>
2019-12-05 09:54:08 +0800

    constructionManager.js constructionManager1.js

--- frontend/MSS.API.UI/src/router/constructionManager.js
+++ frontend/MSS.API.UI/src/router/constructionManager.js
@@ -50,27 +50,6 @@ const routes = [
             component: () => import(/* webpackChunkName: "constructionManager" */ '@/views/constructionManager/monthDetail/children/update.vue')
           }
         ]
-      }, 
-      {
-        path: 'ConstructionPlan',
-        component: () => import( '@/views/workflow/process/index.vue'),
-        children: [
-          {
-            path: '/',
-            name: 'ConstructionPlan',
-            redirect: 'list'
-          }, 
-          {
-            path: 'list',
-            name: 'SeeConstructionPlan',
-            component: () => import(/* webpackChunkName: "system" */ '@/views/workflow/process/children/SeeConstructionPlan.vue')
-          }, 
-          {
-            path: 'add',
-            name: 'AddConstructionPlan',
-            component: () => import(/* webpackChunkName: "system" */ '@/views/workflow/process/children/AddConstructionPlan.vue')
-          }
-        ]
       }
     ]
   }
