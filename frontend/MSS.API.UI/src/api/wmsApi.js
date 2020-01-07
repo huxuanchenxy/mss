477c7661934da9130e5131b71d0f4cddd935ec48
twg <twg>
2020-01-07 16:41:51 +0800

    IP修正

--- frontend/MSS.API.UI/src/api/wmsApi.js
+++ frontend/MSS.API.UI/src/api/wmsApi.js
@@ -1,6 +1,6 @@
 import axios from './interceptors'
-let api = 'http://localhost:3851/api/v1'
-// let api = 'http://10.89.36.154:5801/wmsapi'
+// let api = 'http://localhost:3851/api/v1'
+let api = 'http://10.89.36.154:5801/wmsapi'
 export default {
   // 仓库
   getWarehouse: parm => { return axios.get(`${api}/Warehouse`, {params: parm}).then(res => res.data) },
