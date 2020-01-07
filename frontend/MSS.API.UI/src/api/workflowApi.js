7691cbf4c3410fa6613ae44630f61ef9461fc5b0
twg <twg>
2020-01-07 16:43:43 +0800

    IP修正

--- frontend/MSS.API.UI/src/api/workflowApi.js
+++ frontend/MSS.API.UI/src/api/workflowApi.js
@@ -1,7 +1,7 @@
 import axios from './interceptors'
-let api = 'http://localhost:8443/api/v1'
+// let api = 'http://localhost:8443/api/v1'
 // let api = 'http://localhost:3851/api/v1'
-// let api = 'http://10.89.36.154:5801/workflowapi'
+let api = 'http://10.89.36.154:5801/workflowapi'
 export default {
   getPage: parm => { return axios.get(`${api}/Wf/QueryReadyTasks`, {params: parm}).then(res => res.data) },
   getMyApplyPage: parm => { return axios.get(`${api}/Wf/GetPageMyApply`, {params: parm}).then(res => res.data) },
